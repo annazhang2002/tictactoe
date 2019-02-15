19d6ff0cacf050a1dec16f7eed43d02ccafef81e
Guiling Zhao <itsmombearzhang@gmail.com>
2019-02-14 23:10:22 -0800

    Initial Commit

---
+++ ATown Connect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ATown Connect
+//
+//  Created by Anna Zhang on 2/14/19.
+//  Copyright © 2019 Anna Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
